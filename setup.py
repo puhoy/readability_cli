cd3f2d15ade96bebe645f72f1d1d61828eaa7bfd
jan <jan@kwoh.de>
2020-10-20 23:04:13 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='readability_cli',
     version='0.1',
     py_modules=['readability_cli'],
+    packages=['readability_cli'],
     install_requires=[
         'click==7.1.2',
         'markdownify==0.5.3',
