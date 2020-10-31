2c8c8ac86cb7714e7cef2f814215257cdbf95415
jan <jan@kwoh.de>
2020-10-31 12:23:16 +0100

    add user agent

--- readability_cli/app.py
+++ readability_cli/app.py
@@ -7,7 +7,8 @@ from readability import Document
 @click.command()
 @click.argument('url')
 def cli(url):
-    response = requests.get(url)
+    headers = {'User-Agent': 'readability_cli 0.1'}
+    response = requests.get(url, headers=headers)
     doc = Document(response.text)
     click.echo(html2text.html2text(doc.summary()))
 
